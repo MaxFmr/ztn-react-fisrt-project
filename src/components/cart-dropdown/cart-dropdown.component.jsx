c9b17c3213af29bdb8bade6b1306f0c7e5b5cb83
Maxime <maxime@Maximes-MacBook-Air.local>
2022-02-19 17:59:32 +0100

    added cartc carticon and cartDropdown components, also implementing the toggle state with redux

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,13 @@
+import CustomButton from "../customp-button/custom-button.component";
+import "./cart-dropdown.styles.scss";
+
+const CartDropDown = () => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items"></div>
+      <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+  );
+};
+
+export default CartDropDown;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,8 +3,10 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "/Users/maxime/Desktop/project-1/src/assets/crown.svg";
 import { auth } from "../../firebase/firebase.util";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropDown from "../cart-dropdown/cart-dropdown.component";
 
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -26,13 +28,16 @@ const Header = ({ currentUser }) => {
             SIGN IN
           </Link>
         )}
+        <CartIcon />
       </div>
+      {hidden ? null : <CartDropDown />}{" "}
     </div>
   );
 };
 
-const mapStateToProps = (state) => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
