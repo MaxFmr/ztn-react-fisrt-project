d433c55db36a3a6ff03762079586237c217e2682
Maxime <maxime@Maximes-MacBook-Air.local>
2022-02-28 15:07:49 +0100

    add data to collections page per type of collections

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -6,6 +6,10 @@ import { selectCollections } from "../../redux/shop/shop.selectors";
 
 import { useParams } from "react-router-dom";
 
+import CollectionItem from "../../components/collection-item/collectionItem.component";
+
+import "./collection.styles.scss";
+
 const Collection = ({ collections }) => {
   const { routeName } = useParams();
 
@@ -14,8 +18,13 @@ const Collection = ({ collections }) => {
       {collections.map((collection) => {
         return (
           routeName === collection.routeName && (
-            <div key={collection.id}>
-              <h2>{collection.title}</h2>
+            <div key={collection.id} className="collection-page">
+              <h2 className="title">{collection.title}</h2>
+              <div className="items">
+                {collection.items.map((item) => (
+                  <CollectionItem key={item.id} item={item} />
+                ))}
+              </div>
             </div>
           )
         );
--- src/pages/shoppage/shop.component.jsx
+++ src/pages/shoppage/shop.component.jsx
@@ -4,11 +4,7 @@ import { createStructuredSelector } from "reselect";
 
 import { connect } from "react-redux";
 
-import { selectCollections } from "../../redux/shop/shop.selectors";
-
-import { Link } from "react-router-dom";
-
-const ShopPage = () => {
+const ShopPage = ({}) => {
   return (
     <div className="shop-page">
       <CollectionsOverview />
@@ -16,8 +12,4 @@ const ShopPage = () => {
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
